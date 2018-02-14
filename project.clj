616f32b73e2fac3fef42633ce214ae7d10c1aff5
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-02-14 23:02:42 +0100

    good

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jvm -opts ["-Djava.library.path=native/"]
+  :jvm-opts [~(str "-Djava.library.path=native/:"
+                   (System/getenv "LD_LIBRARY_PATH"))]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [net.n01se/clojure-jna "1.0.0"]])
